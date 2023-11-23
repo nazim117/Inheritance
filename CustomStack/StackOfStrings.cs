850a8df967bb6c0e01a220c28f687f011afdfc95
Nazim Ahmedov <61702781+nazim117@users.noreply.github.com>
2023-11-23 23:46:59 +0100

    Add files via upload

---
+++ CustomStack/StackOfStrings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool isEmpty()
+        {
+            return this.Count == 0;
+        }
+        public Stack<string> AddRange() 
+        {
+            return this;
+        }
+    }
+}
